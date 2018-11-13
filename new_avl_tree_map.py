3d97c2055c4216839c972c2745f7084c3b283d7e
Francesco de Pertis <f.depertis@studenti.unisa.it>
2018-11-13 19:31:26 +0100

    Update new_avl_tree_map.py

--- new_avl_tree_map.py
+++ new_avl_tree_map.py
@@ -1,1 +1,14 @@
+from TdP_collections.map.avl_tree import TreeMap
+
+class NewAVLTreeMap(TreeMap):
+
+    class _Node(TreeMap._Node):
+        __slots__ = '_balance_factor'
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            super().__init__(element, parent, left, right)
+            self._balance_factor = 0
+
+
+
 
