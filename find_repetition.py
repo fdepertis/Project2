f0996c7f360c6c9223d0e85cdcf383e5f5cbe75a
Valentino-Vastola <valedx93@gmail.com>
2018-11-15 17:15:49 +0100

    Update find_repetition.py
    funzione per i file duplicati ma non con complessità o(n)

--- find_repetition.py
+++ find_repetition.py
@@ -1,1 +1,32 @@
 
+from os import listdir
+path = "/Users/VALE/Desktop/testi/"
+lista=[]
+
+k=0
+list=listdir(path)
+size =list.__len__()-1
+
+for val in listdir(path):
+    file = open(path+val,"r")
+    content = file.read()
+    lista.insert(k,content)
+    k+=1
+    file.close()
+
+
+duplicate =[]
+
+
+d=0
+for i,val in enumerate(lista):
+    for j,val2 in enumerate(lista):
+        if(val == val2):
+            if (list.__getitem__(i) in duplicate) == 0 and i!=j :
+               testo = list.__getitem__(j)+" e' uguale a "+list.__getitem__(i)
+               duplicate.insert(d,testo)
+               d+=1
+
+
+
+print(duplicate)
