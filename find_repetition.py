bd473ecb338c4634b4690aa375915dcd0f0793d2
Valentino-Vastola <valedx93@gmail.com>
2018-11-15 18:08:59 +0100

    Update find_repetition.py

--- find_repetition.py
+++ find_repetition.py
@@ -1,32 +1,33 @@
 
 from os import listdir
-path = "/Users/VALE/Desktop/testi/"
-lista=[]
 
-k=0
-list=listdir(path)
-size =list.__len__()-1
+def find_repetition(path):
+    lista=[]
 
-for val in listdir(path):
-    file = open(path+val,"r")
-    content = file.read()
-    lista.insert(k,content)
-    k+=1
-    file.close()
+    k=0
+    list=listdir(path)
+    size =list.__len__()-1
 
-
-duplicate =[]
+    for val in listdir(path):
+        file = open(path+val,"r")
+        content = file.read()
+        lista.insert(k,content)
+        k+=1
+        file.close()
 
 
-d=0
-for i,val in enumerate(lista):
-    for j,val2 in enumerate(lista):
-        if(val == val2):
-            if (list.__getitem__(i) in duplicate) == 0 and i!=j :
-               testo = list.__getitem__(j)+" e' uguale a "+list.__getitem__(i)
-               duplicate.insert(d,testo)
-               d+=1
+    duplicate =[]
+
+
+    d=0
+    for i,val in enumerate(lista):
+        for j,val2 in enumerate(lista):
+            if(val == val2):
+                if (list.__getitem__(i) in duplicate) == 0 and i!=j :
+                   testo = list.__getitem__(j)+" e' uguale a "+list.__getitem__(i)
+                   duplicate.insert(d,testo)
+                   d+=1
 
 
 
-print(duplicate)
+    return duplicate
