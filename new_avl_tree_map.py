c0c26c1fb85783339655b229b7ba668184c87b6c
Francesco de Pertis <f.depertis@studenti.unisa.it>
2018-11-17 14:51:25 +0100

    Update new_avl_tree_map.py
    Riorganizzato l'algoritmo, aggiunti commenti e un piccolo test. Si richiedono maggiori test di verifica.

--- new_avl_tree_map.py
+++ new_avl_tree_map.py
@@ -13,46 +13,113 @@ class NewAVLTreeMap(TreeMap):
     def _isbalanced(self, p):
         return -1 <= p._node._balance_factor <= 1
 
-    def _node_height(self, p):
-        node = self._validate(p)
-        if node._left is None and node._right is None:
-            return 1
-        elif node._left is None:
-            return 1 + self._node_height(node._right)
-        elif node._right is None:
-            return 1 + self._node_height(node._left)
+    def _recompute_balance_factor(self, x):
+        y = self.parent(x)
+        z = self.parent(y)
+
+        #Primo caso
+        if x == self.right(y) and y == self.right(z):
+            # x rimane invariato
+            y._node._balance_factor = 0
+            if self.left(y) is None:
+                if self.left(z) is None:
+                    z._node._balance_factor = 0
+                else:
+                    z._node._balance_factor = 1
+            else:
+                if self.left(z) is None:
+                    z._node._balance_factor = -1
+                else:
+                    z._node._balance_factor = 0
+
+        #Secondo caso
+        elif x == self.left(y) and y == self.left(z):
+            #x rimane invariato
+            y._node._balance_factor = 0
+            if self.right(y) is None:
+                if self.right(z) is None:
+                    z._node._balance_factor = 0
+                else:
+                    z._node._balance_factor = -1
+            else:
+                if self.right(z) is None:
+                    z._node._balance_factor = 1
+                else:
+                    z._node._balance_factor = 0
+
+        #Terzo caso
+        elif x == self.left(y) and y == self.right(z):
+            x._node._balance_factor = 0
+            if self.right(x) is None and self.left(x) is None:
+                y._node._balance_factor = 0
+                z._node._balance_factor = 0
+            elif self.right(x) is None:
+                if self.right(y) is None:
+                    y._node._balance_factor = 0
+                else:
+                    y._node._balance_factor = -1
+                if self.left(z) is None:
+                    z._node._balance_factor = -1
+                else:
+                    z._node._balance_factor = 0
+            else: #self.left(x) is None:
+                if self.right(y) is None:
+                    y._node._balance_factor = 1
+                else:
+                    y._node._balance_factor = 0
+                if self.left(z) is None:
+                    z._node._balance_factor = 0
+                else:
+                    z._node._balance_factor = 1
+
+        #Quarto caso, cioè se x == self.right(y) and y == self.left(z)
         else:
-            return 1 + max(self._node_height(node._left), self._node_height(node._right ))
+            x._node._balance_factor = 0
+            if self.right(x) is None and self.left(x) is None:
+                y._node._balance_factor = 0
+                z._node._balance_factor = 0
+            elif self.right(x) is None:
+                if self.left(y) is None:
+                    y._node._balance_factor = 1
+                else:
+                    y._node._balance_factor = 0
+                if self.right(z) is None:
+                    z._node._balance_factor = 0
+                else:
+                    z._node._balance_factor = -1
+            else:  # self.left(x) is None:
+                if self.left(y) is None:
+                    y._node._balance_factor = 0
+                else:
+                    y._node._balance_factor = 1
+                if self.right(z) is None:
+                    z._node._balance_factor = -1
+                else:
+                    z._node._balance_factor = 0
+
+
     # ---------------------------- override balancing hooks -----------------------
     def _rebalance_insert(self, p):
-        print("\nè stato inserito", p.element()._value)
         save = None
         cursor = p
         while self.parent(cursor) is not None:
             up_cursor = self.parent(cursor)
             if cursor == self.left(up_cursor):
                 up_cursor._node._balance_factor += 1
-                print("up_cursor._node._balance_factor", up_cursor.element()._value, up_cursor._node._balance_factor)
             else:
                 up_cursor._node._balance_factor -= 1
-                print("up_cursor._node._balance_factor", up_cursor.element()._value, up_cursor._node._balance_factor)
+
             if not self._isbalanced(up_cursor):
-                print("Trovato nodo non bilanciato")
-                save._node._balance_factor = 0
-                cursor._node._balance_factor = 0
-                up_cursor._node._balance_factor = 0
-                print("Up Cursor:", up_cursor.element()._value)
-                print("Cursor:", cursor.element()._value)
-                print("Save:", save.element()._value)
+                self._recompute_balance_factor(save)
                 self._restructure(save)
-                print("Rotazione effettuata")
             if up_cursor._node._balance_factor == 0:
                 cursor = self.root()
             else:
                 save = cursor
                 cursor = self.parent(cursor)
 
-    """def _rebalance_delete(self, p):
+    def _rebalance_delete(self, p):
+        save = None
         cursor = p
         while self.parent(cursor) is not None:
             up_cursor = self.parent(cursor)
@@ -60,45 +127,20 @@ class NewAVLTreeMap(TreeMap):
                 up_cursor._node._balance_factor -= 1
             else:
                 up_cursor._node._balance_factor += 1
+
             if not self._isbalanced(up_cursor):
-                print("Trovato nodo non bilanciato")
-                cursor._node._balance_factor = 0
-                up_cursor._node._balance_factor = 0
-                self._restructure(p)
-                print("Rotazione effettuata")
+                self._recompute_balance_factor(save)
+                self._restructure(save)
             if up_cursor._node._balance_factor == 0:
-                cursor = None
+                cursor = self.root()
             else:
-                cursor = self.parent(cursor)"""
+                save = cursor
+                cursor = self.parent(cursor)
 
 if __name__ == '__main__':
     a = NewAVLTreeMap()
-    a[0] = 0
-    a[1] = 1
-    a[2] = 2
-    a[3] = 3
-    a[4] = 4
-    a[5] = 5
-    a[6] = 6
-
-    print("\tSTAMPA!")
-    print("\t  ",a.root().element()._value)
-    print("\t", a.left(a.root()).element()._value, "\t", a.right(a.root()).element()._value)
-
-    if a.left(a.left(a.root())) is not None:
-        print(a.left(a.left(a.root())).element()._value,end='\t')
-    else:
-        print("N",end='\t')
-    if a.right(a.left(a.root())) is not None:
-        print(a.right(a.left(a.root())).element()._value,end='\t')
-    else:
-        print("N",end='\t')
-    if a.left(a.right(a.root())) is not None:
-        print(a.left(a.right(a.root())).element()._value,end='\t')
-    else:
-        print("N",end='\t')
-    if a.right(a.right(a.root())) is not None:
-        print(a.right(a.right(a.root())).element()._value,end='\n')
-    else:
-        print("N",end='\t')
-#    print(a.right(a.right(a.right(a.root()))).element()._value)
+    for i in range(0, 101):
+        a[i] = i
+    
+    for i in a.inorder():
+        print(i.element()._value, "\t", i._node._balance_factor)
