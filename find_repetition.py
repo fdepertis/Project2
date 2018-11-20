b1c113843903dd9ad368f696a67bbaeae6789385
Valentino-Vastola <valedx93@gmail.com>
2018-11-20 12:38:56 +0100

    Update find_repetition
    Ragazzi una soluzione che credo di aver trovato è quella di usare un dizionario datemi un vostro parere

--- find_repetition.py
+++ find_repetition.py
@@ -1,33 +1,35 @@
-
 from os import listdir
 
 def find_repetition(path):
-    lista=[]
-
+    dict=[]
     k=0
     list=listdir(path)
-    size =list.__len__()-1
 
     for val in listdir(path):
         file = open(path+val,"r")
         content = file.read()
-        lista.insert(k,content)
+        dict.insert(k,content)
         k+=1
         file.close()
 
-
     duplicate =[]
+    testListDict = {}
+    l=0
+    m=0
 
+    for item in dict:
+        if item not in testListDict:
+           testListDict[item]=1
 
-    d=0
-    for i,val in enumerate(lista):
-        for j,val2 in enumerate(lista):
-            if(val == val2):
-                if (list.__getitem__(i) in duplicate) == 0 and i!=j :
-                   testo = list.__getitem__(j)+" e' uguale a "+list.__getitem__(i)
-                   duplicate.insert(d,testo)
-                   d+=1
-
-
+        else:
+            testListDict[item]+=1
+            duplicate.insert(m,list.__getitem__(l))
+            m+=1
+        l+=1
 
     return duplicate
+
+
+path = "/Users/VALE/Desktop/testi/"
+
+print(find_repetition(path))
