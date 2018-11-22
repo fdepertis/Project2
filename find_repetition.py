db0d30bd10de3fe672e011df97ebccd99faf2ac3
Valentino-Vastola <valedx93@gmail.com>
2018-11-22 15:14:45 +0100

    Update find_repetition.py

--- find_repetition.py
+++ find_repetition.py
@@ -1,31 +1,32 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 from os import listdir
 
 
 
 def find_repetition(path):
-
     dup = []
     duplicate = {}
     fileset = set()
 
     for val in listdir(path):
-        file = open(path+val,"r")
-        c = hash(file.read())
-        file.close()
-        if c in fileset:
-            duplicate[c].append(val)
+        file = open(path + val, "r")  # apre il file
+        c = hash(file.read())  # hash associato al testo del file letto
+
+        file.close()  # chiudo il flusso del file
+        if c in fileset:  # vedo  se l'hash è gia presente nel set ...usiamo i set perchè la ricerca ha complessità O(1)
+            duplicate[c].append(val)  # append con chiave valore su duplicare che è un dizionario
         else:
-            fileset.add(c)
-            duplicate[c]=[val]
-        
+            fileset.add(c)  # se non è gia presente nell'hash aggiungo a duplicate la coppia chiave valore nuova
+            duplicate[c] = [val]
 
-    for val in duplicate.values():
-        if(len(val)>1):
+    for val in duplicate.values(): # serve per inserire in dup ( ovvero il vettore che ritorneremo) solo quelli con occorrenze maggiori di 1
+        if (len(val) > 1):
             dup.append(val)
-
 
     return list(dup)
 
 
-path = "/Users/VALE/Desktop/testi/"
+path = "../Progetto2/testi/"
 print(find_repetition(path))
