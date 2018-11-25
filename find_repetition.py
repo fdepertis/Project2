125de5e991f3eec9bc5f2012d02ac370abd05f5a
Francesco de Pertis <f.depertis@studenti.unisa.it>
2018-11-25 23:33:52 +0100

    a

--- find_repetition.py
+++ find_repetition.py
@@ -1,24 +1,23 @@
+from TdP_collections.hash_table.probe_hash_map import ProbeHashMap
 from os import listdir
 
 def find_repetition(dir):
-    dup = list()
-    duplicate = dict()
-    fileset = set()
+    hashmap = ProbeHashMap()
+    duplicate = list()
 
     for val in listdir(dir):
-        file = open(dir + val, "r")    # apre il file
+        file = open(dir + val, "r")     # apre il file
         c = hash(file.read())           # hash associato al testo del file letto
         file.close()                    # chiudo il flusso del file
-        if c in fileset:                # vedo se l'hash è gia presente nel set, usiamo i set perché la ricerca ha complessità O(1)
-            duplicate[c].append(val)    # append con chiave valore su duplicare che è un dizionario
+        if c not in hashmap:            # vedo se l'hash è gia presente nel set, usiamo i set perché la ricerca ha complessità O(1)
+            hashmap[c] = [val]          # append con chiave valore su duplicare che è un dizionario
         else:
-            fileset.add(c)              # se non è gia presente nell'hash aggiungo a duplicate la coppia chiave valore nuova
-            duplicate[c] = [val]
+            hashmap[c].append(val)      # se non è gia presente nell'hash aggiungo a duplicate la coppia chiave valore nuova
 
-    for val in duplicate.values():      # serve per inserire in dup (ovvero il vettore che ritorneremo) solo quelli con occorrenze maggiori di 1
-        if (len(val) > 1):
-            dup.append(val)
-    return dup
+    for l in hashmap.values():          # serve per inserire in dup (ovvero il vettore che ritorneremo) solo quelli con occorrenze maggiori di 1
+        if (len(l) > 1):
+            duplicate.append(l)
+    return duplicate
 
 if __name__ == '__main__':
     print("Lista dei file duplicati nella directory 'dir'")
