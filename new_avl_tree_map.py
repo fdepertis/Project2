50f05fa9b57dc41840b3668d07caa8f2ec6072ca
Antonino95 <43966341+Antonino95@users.noreply.github.com>
2018-11-25 20:27:56 +0100

    Add files via upload

--- new_avl_tree_map.py
+++ new_avl_tree_map.py
@@ -7,68 +7,78 @@ class NewAVLTreeMap(TreeMap):
         __slots__ = '_balance_factor'
 
         def __init__(self, element, parent=None, left=None, right=None):
+
             super().__init__(element, parent, left, right)
             self._balance_factor = 0
 
     def _isbalanced(self, p):
+
         node = self._validate(p)
         return -1 <= node._balance_factor <= 1
 
     def _rebalance(self, p):
-        #print("p:",p.element()._value,"|",p._node._balance_factor,"left(p):",self.left(p),"right",self.right(p))
+
         if p._node._balance_factor < 0:
 
             if self.right(p)._node._balance_factor > 0:
 
-                self._rotate(self.left(self.right(p)))         #right
+                self._rotate(self.left(self.right(p)))         #rotate to right
                 """Rotazione effettuata e ora è allineata"""
                 self.right(self.right(p))._node._balance_factor += -1 - max(self.right(p)._node._balance_factor, 0)
                 self.right(p)._node._balance_factor += -1 + min(self.right(self.right(p))._node._balance_factor, 0)
 
-                self._rotate(self.right(p))         #left
+                self._rotate(self.right(p))                     #rotate to left
                 p._node._balance_factor += 1 - min(self.parent(p)._node._balance_factor, 0)
                 self.parent(p)._node._balance_factor += 1 + max(p._node._balance_factor, 0)
 
             else:
-
-                self._rotate(self.right(p))         #left
+                self._rotate(self.right(p))                     #rotate to left
                 p._node._balance_factor += 1 - min(self.parent(p)._node._balance_factor, 0)
                 self.parent(p)._node._balance_factor += 1 + max(p._node._balance_factor, 0)
-
-                #print(p.element()._value,"|",p._node._balance_factor,self.parent(p).element()._value,"|",self.parent(p)._node._balance_factor)
                 return self.parent(p)
-                #self._rebalance_delete_auxiliary(self.parent(p),self.parent(self.parent(p)))
 
 
-        elif p._node._balance_factor > 0: #else
+        elif p._node._balance_factor > 0:
 
             if self.left(p)._node._balance_factor < 0:
-
-                self._rotate(self.right(self.left(p)))          #left #right(left(p))
+                self._rotate(self.right(self.left(p)))          #rotate left
                 """Rotazione effettuata e ora è allineata"""
                 self.left(self.left(p))._node._balance_factor += 1 - min(self.left(p)._node._balance_factor, 0)
                 self.left(p)._node._balance_factor += 1 + max(self.left(self.left(p))._node._balance_factor, 0)
 
-                self._rotate(self.left(p))          #right
+                self._rotate(self.left(p))                      #rotate right
                 p._node._balance_factor += -1 - max(self.parent(p)._node._balance_factor, 0)
                 self.parent(p)._node._balance_factor += -1 + min(p._node._balance_factor, 0)
 
             else:
-
-                #print(self.left(p).element()._value,"|",self.left(p)._node._balance_factor,p.element()._value,"|",p._node._balance_factor)
-                self._rotate(self.left(p))          #right
-
-                #print(p.element()._value,"|",p._node._balance_factor,self.parent(p).element()._value,"|",self.parent(p)._node._balance_factor)
+                self._rotate(self.left(p))                      #rotate right
                 p._node._balance_factor += -1 - max(self.parent(p)._node._balance_factor, 0)
                 self.parent(p)._node._balance_factor += -1 + min(p._node._balance_factor, 0)
-                #print(p.element()._value,"|",p._node._balance_factor,self.parent(p).element()._value,"|",self.parent(p)._node._balance_factor)
                 return self.parent(p)
+
+    def _rebalance_delete_auxiliary(self, p, parent):
+
+        if parent is not None:
+
+            if p == self.left(parent):
+                parent._node._balance_factor -= 1
+            else:
+                parent._node._balance_factor += 1
+
+            if not self._isbalanced(parent):
+                self._rebalance(parent)
+                if update_p is not None:
+                    p = update_p
+                else:
+                    return
+
+            if parent._node._balance_factor == 0:
+                self._rebalance_delete_auxiliary(parent, self.parent(parent))
 
     # ---------------------------- override balancing hooks -----------------------
     def _rebalance_insert(self, p):
 
         if not self._isbalanced(p):
-
             self._rebalance(p)
             return
 
@@ -76,87 +86,56 @@ class NewAVLTreeMap(TreeMap):
 
         if parent is not None:
 
-
             if p == self.left(parent):
-
                 parent._node._balance_factor += 1
 
             else:
-
                 parent._node._balance_factor -= 1
 
             if parent._node._balance_factor != 0:
-
                 self._rebalance_insert(parent)
 
     def _rebalance_delete(self, p):
 
         if not p:
-
             return
 
         elif not self.left(p) and not self.right(p):
-
             p._node._balance_factor = 0
 
         elif not self.left(p):
-
             p._node._balance_factor -= 1
 
         elif not self.right(p):
-
             p._node._balance_factor += 1
 
         else:
             if self.num_children(self.left(p)) < self.num_children(self.right(p)):
-
                 p._node._balance_factor -= 1
 
             elif self.num_children(self.left(p)) > self.num_children(self.right(p)):
-
                 p._node._balance_factor += 1
 
             elif self.num_children(self.left(p)) == self.num_children(self.right(p)) == 0:
-
                 p._node._balance_factor = 0
 
         if not self._isbalanced(p):
             update_p=self._rebalance(p)
             if update_p is not None:
                 p=update_p
-
-            #print(p.element()._value,"|",p._node._balance_factor,self.parent(p).element()._value,"|",self.parent(p)._node._balance_factor)
-
+            else:
+                return
 
         if p._node._balance_factor == 0:
             self._rebalance_delete_auxiliary(p, self.parent(p))
 
-    def _rebalance_delete_auxiliary(self, p, parent):
-
-        if parent is not None:
-
-            if p == self.left(parent):
-
-                parent._node._balance_factor -= 1
-
-            else:
-
-                parent._node._balance_factor += 1
-
-            if not self._isbalanced(parent):
-
-                self._rebalance(parent)
-                return
-
-            if parent._node._balance_factor == 0:
-
-                self._rebalance_delete_auxiliary(parent, self.parent(parent))
 
 if __name__ == '__main__':
     a = NewAVLTreeMap()
     for i in range(1, 16):
         a[i] = i
     print("------------------------Insert----------------------------")
+    print("Node|Balance Factor")
     for k in a.inorder():
         print(k.value(),"|",k._node._balance_factor)
     print("------------------------Insert----------------------------")
@@ -165,12 +144,14 @@ if __name__ == '__main__':
         print("Elimino--->",a[i])
         del a[i]
         print("----------------------------------------------------------")
+        print("Node|Balance Factor")
         for k in a.inorder():
             print(k.value(),"|",k._node._balance_factor)
         print("----------------------------------------------------------")
     for i in range(1, 18):
         a[i] = i
     print("------------------------Insert----------------------------")
+    print("Node|Balance Factor")
     for k in a.inorder():
         print(k.value(),"|",k._node._balance_factor)
     print("------------------------Insert----------------------------")
@@ -179,6 +160,7 @@ if __name__ == '__main__':
         print("Elimino--->",a[i])
         del a[i]
         print("----------------------------------------------------------")
+        print("Node|Balance Factor")
         for k in a.inorder():
             print(k.value(),"|",k._node._balance_factor)
         print("----------------------------------------------------------")
@@ -186,6 +168,7 @@ if __name__ == '__main__':
     for i in range(1, 16):
         a[i] = i
     print("------------------------Insert----------------------------")
+    print("Node|Balance Factor")
     for k in a.inorder():
         print(k.value(),"|",k._node._balance_factor)
     print("------------------------Insert----------------------------")
@@ -194,6 +177,7 @@ if __name__ == '__main__':
         print("Elimino--->",a[i])
         del a[i]
         print("----------------------------------------------------------")
+        print("Node|Balance Factor")
         for k in a.inorder():
             print(k.value(),"|",k._node._balance_factor)
         print("----------------------------------------------------------")
